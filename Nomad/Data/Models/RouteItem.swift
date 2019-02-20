3e724c632e31c8bebf57e484d5d3dacf853a96c5
Mohammed Safwat <mail.msafwat@gmail.com>
2019-02-20 08:56:01 +0100

    Create a new 'RouteItem' struct.

---
+++ Nomad/Data/Models/RouteItem.swift
@@ -0,0 +1,22 @@
+//
+//  RouteItem.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 20.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+import Foundation
+
+struct RouteItem {
+    
+    // MARK: - Properties
+    
+    var flyTo: String
+    var flyFrom: String
+    var cityTo: String
+    var cityFrom: String
+    var departureTime: TimeInterval
+    var arrivalTime: TimeInterval
+    var flightNumber: Int
+}
