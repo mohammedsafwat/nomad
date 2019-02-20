7806754f52e2bdeabec140bc0954cdbafe9ae6f7
Mohammed Safwat <mail.msafwat@gmail.com>
2019-02-20 08:57:26 +0100

    Add all default crated files for 'Nomad' project.

---
+++ Nomad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 19.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NomadTests/NomadTests.swift
@@ -0,0 +1,34 @@
+//
+//  NomadTests.swift
+//  NomadTests
+//
+//  Created by Mohammed Safwat on 19.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+import XCTest
+@testable import Nomad
+
+class NomadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
