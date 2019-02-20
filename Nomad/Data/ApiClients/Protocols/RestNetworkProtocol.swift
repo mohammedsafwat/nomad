5ea7e45b29a3d03d4db4885d881acb79a3a65ac2
Mohammed Safwat <mail.msafwat@gmail.com>
2019-02-20 08:56:26 +0100

    Create a new protocol 'RestNetworkProtocol' for all RestClient operations.

---
+++ Nomad/Data/ApiClients/Protocols/RestNetworkProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  RestNetworkProtocol.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 20.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+protocol RestNetworkProtocol {
+    
+}
