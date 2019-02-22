dd5d20d697c1145750a1eee165b82c8f88740f1c
Mohammed Safwat <mail.msafwat@gmail.com>
2019-02-22 09:50:50 +0100

    Create a new enum 'HTTPRequestType'.

---
+++ Nomad/Data/Helpers/HTTPRequestType.swift
@@ -0,0 +1,12 @@
+//
+//  HTTPRequestType.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 21.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+enum HTTPRequestType {
+    case get
+    case post
+}
