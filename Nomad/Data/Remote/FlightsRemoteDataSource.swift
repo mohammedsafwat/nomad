a18e543346106763d3bc9692104beeda26f85878
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-22 17:45:18 +0100

    Create a new class for 'FlightsRemoteDataSource'.

--- Nomad/Data/Remote/FlightsRemoteDataSource.swift
+++ Nomad/Data/Remote/FlightsRemoteDataSource.swift
@@ -7,6 +7,7 @@
 //
 
 import RxSwift
+import Alamofire
 
 class FlightsRemoteDataSource: FlightsDataSource {
     
@@ -22,17 +23,45 @@ class FlightsRemoteDataSource: FlightsDataSource {
     
     // MARK: - FlightsDataSource
     
-    func flights(from: String, to: String, dateFrom: String, dateTo: String) -> Observable<[Flight]> {
-        return restNetworkClient.performRequest(requestURLString: API.flightsUrl, type: .get).flatMap({ data in
-            try self.parseFlightsQueryResponse(data: data)
-        })
+    func flights(flightsFilter: FlightsFilter) -> Observable<[Flight]> {
+        let from = flightsFilter.from
+        let dateFrom = flightsFilter.dateFrom
+        let dateTo = flightsFilter.dateTo
+        let limit = flightsFilter.limit
+        let partner = Constants.DefaultFilter.partner
+
+        let parameters: [String: Any] = [FlightsRequestParameter.from.rawValue: from,
+                                         FlightsRequestParameter.dateFrom.rawValue: dateFrom,
+                                         FlightsRequestParameter.dateTo.rawValue: dateTo,
+                                         FlightsRequestParameter.limit.rawValue: limit,
+                                         FlightsRequestParameter.partner.rawValue: partner]
+
+        return restNetworkClient.performRequest(requestURLString: API.Flights.endpointUrl, type: .get, parameters: parameters).flatMap { result in
+            try self.parseFlightsQueryResponse(result: result)
+        }
     }
 }
 
 // MARK: - Parsing Methods
 
 extension FlightsRemoteDataSource {
-    private func parseFlightsQueryResponse(data: [String: Any]) throws -> Observable<[Flight]> {
-        return Observable<[Flight]>.of([])
+    private func parseFlightsQueryResponse(result: Result<(HTTPURLResponse, Any), DataError>) throws -> Observable<[Flight]> {
+        switch result {
+        case .success(let response):
+            guard let responseData = response.1 as? [String: Any],
+                let flightsData = responseData[API.Flights.ResponseKeys.data] as? [[String: Any]] else {
+                    throw DataErrorHelper.parseError
+            }
+            let flights = flightsData.compactMap { flightData in
+                return self.parseFlight(flightData: flightData)
+            }.compactMap { $0 }
+            return Observable<[Flight]>.of(flights)
+        case .failure(let error):
+            throw error
+        }
+    }
+
+    private func parseFlight(flightData: [String: Any]) -> Flight? {
+        return Flight(JSON: flightData)
     }
 }
