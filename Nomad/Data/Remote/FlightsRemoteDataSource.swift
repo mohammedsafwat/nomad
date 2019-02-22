31e540f5133b7f3f193589001ebf996ac0d683ac
Mohammed Safwat <mail.msafwat@gmail.com>
2019-02-22 09:47:56 +0100

    Add a new implementation for 'FlightsRemoteDataSource'.

--- Nomad/Data/Remote/FlightsRemoteDataSource.swift
+++ Nomad/Data/Remote/FlightsRemoteDataSource.swift
@@ -17,6 +17,22 @@ class FlightsRemoteDataSource: FlightsDataSource {
     // MARK: - Initializer
     
     init(restNetworkClient: RestNetworkClientProtocol) {
-        
+        self.restNetworkClient = restNetworkClient
+    }
+    
+    // MARK: - FlightsDataSource
+    
+    func flights(from: String, to: String, dateFrom: String, dateTo: String) -> Observable<[Flight]> {
+        return restNetworkClient.performRequest(requestURLString: API.flightsUrl, type: .get).flatMap({ data in
+            try self.parseFlightsQueryResponse(data: data)
+        })
+    }
+}
+
+// MARK: - Parsing Methods
+
+extension FlightsRemoteDataSource {
+    private func parseFlightsQueryResponse(data: [String: Any]) throws -> Observable<[Flight]> {
+        return Observable<[Flight]>.of([])
     }
 }
