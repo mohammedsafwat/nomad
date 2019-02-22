39ba1d0f17555b428638c044e9f24ea1f53b174a
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-22 17:47:20 +0100

    Add a new 'Constants' struct to hold the constants values related to the whole app.

---
+++ Nomad/App/Constants/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 22.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct DefaultFilter {
+        static let from = "TXL,SXF"
+        static let dateFrom = ""
+        static let dateTo = ""
+        static let price = 50
+        static let limit = 30
+        static let partner = "skyscanner"
+    }
+
+    struct NavigationBar {
+        static let tintColorHex = "#fff"
+    }
+}
