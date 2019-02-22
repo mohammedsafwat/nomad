16ab4dd6f7f05b975fc715c70c29e097cca1b374
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-22 17:42:15 +0100

    Create a new 'DataError' struct with 'statusCode', 'dataErrorType' and 'dataErrorMessage' properties.

--- Nomad/Data/Models/DataError.swift
+++ Nomad/Data/Models/DataError.swift
@@ -9,6 +9,18 @@
 import Foundation
 
 struct DataError: Error {
-    let dataErrorType: DataErrorType
-    let dataErrorMessage: String?
+
+    // MARK: - Properties
+
+    private let statusCode: Int?
+    private let dataErrorType: DataErrorType
+    private let dataErrorMessage: String?
+
+    // MARK: - Initializer
+
+    init(statusCode: Int? = nil, dataErrorType: DataErrorType, dataErrorMessage: String?) {
+        self.statusCode = statusCode
+        self.dataErrorType = dataErrorType
+        self.dataErrorMessage = dataErrorMessage
+    }
 }
