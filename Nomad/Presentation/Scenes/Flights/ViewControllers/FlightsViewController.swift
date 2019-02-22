83ffac37371761c570ee7c12c06171d24ee1c0ab
Mohammed Safwat <mail.msafwat@gmail.com>
2019-02-22 21:10:51 +0100

    Create a new method 'setupFlightsCollectionView' inside 'FlightsViewController'.

--- Nomad/Presentation/Scenes/Flights/ViewControllers/FlightsViewController.swift
+++ Nomad/Presentation/Scenes/Flights/ViewControllers/FlightsViewController.swift
@@ -13,6 +13,7 @@ class FlightsViewController: UIViewController {
 
     // MARK: - Properties
 
+    @IBOutlet private weak var flightsCollectionView: UICollectionView!
     private let schedulersFacade = SchedulersFacade()
     private let disposeBag = DisposeBag()
 
@@ -27,6 +28,7 @@ class FlightsViewController: UIViewController {
 
         // Setup UI
         setupNavigationBarStyle()
+        setupFlightsCollectionView()
 
         // Data Binding
         viewModel.flights.subscribe(onNext: { flights in
@@ -41,10 +43,25 @@ class FlightsViewController: UIViewController {
     }
 }
 
+// MARK: - FlightsCollectionViewFlowLayout Delegate
+
+extension FlightsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+    }
+}
+
 // MARK: - Private Methods
 
 extension FlightsViewController {
     func setupNavigationBarStyle() {
         self.navigationController?.navigationBar.makeTransparent(withTintColor: UIColor(hexString: Constants.NavigationBar.tintColorHex))
+    }
+    
+    func setupFlightsCollectionView() {
+        let cellNib = UINib(nibName: Constants.ViewControllers.Flights.CollectionView.CellName, bundle: .main)
+        flightsCollectionView.register(cellNib, forCellWithReuseIdentifier: Constants.ViewControllers.Flights.CollectionView.CellIdentifier)
+        flightsCollectionView.delegate = self
+        flightsCollectionView.allowsSelection = false
     }
 }
