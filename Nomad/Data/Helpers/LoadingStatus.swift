7e338c23f87ed16e8478b8502717de6508ca8837
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-22 17:36:38 +0100

    Create a new enum 'LoadingStatus'.

---
+++ Nomad/Data/Helpers/LoadingStatus.swift
@@ -0,0 +1,13 @@
+//
+//  LoadingStatus.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 22.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+enum LoadingStatus {
+    case success
+    case loading
+    case failed
+}
