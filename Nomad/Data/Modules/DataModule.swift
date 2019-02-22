7422033c3b3648b355a0936fcb244f0270c723a5
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-22 17:44:30 +0100

    Create a new singleton class 'FlightsModule'.

---
+++ Nomad/Data/Modules/DataModule.swift
@@ -0,0 +1,21 @@
+//
+//  DataModule.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 22.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+import Foundation
+
+class DataModule {
+    static let shared = DataModule()
+    private let restNetworkClient = HttpClientModule.shared.httpClient
+
+    private init() {}
+
+    func flightsRepository() -> FlightsDataSource {
+        let flightsRemoteDataSource = FlightsModule.shared.flightsRemoteDataSource(restNetworkClient: restNetworkClient)
+        return FlightsRepository(remoteDataSource: flightsRemoteDataSource)
+    }
+}
