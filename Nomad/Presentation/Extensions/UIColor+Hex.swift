8113070b51a9f827d688bd14b640628852878dc0
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-22 17:35:11 +0100

    Add a new 'UIColor' extension to accept a color with a hex string.

---
+++ Nomad/Presentation/Extensions/UIColor+Hex.swift
@@ -0,0 +1,31 @@
+//
+//  UIColor+Hex.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 22.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt32()
+        Scanner(string: hex).scanHexInt32(&int)
+        let a, r, g, b: UInt32
+
+        switch hex.count {
+        case 3:
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6:
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8:
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
