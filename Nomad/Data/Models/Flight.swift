c27fa0ecf0a8fc1ad6bd046a86038e5e37fff8d6
Mohammed Safwat <mail.msafwat@gmail.com>
2019-02-22 09:51:01 +0100

    Create a new 'Flight' model.

--- Nomad/Data/Models/Flight.swift
+++ Nomad/Data/Models/Flight.swift
@@ -7,25 +7,26 @@
 //
 
 import Foundation
+import ObjectMapper
 
-class Flight {
+class Flight: Mappable {
     
     // MARK: - Properties
     
-    var flyTo: String
-    var flyFrom: String
-    var cityTo: String
-    var cityFrom: String
-    var departureTime: TimeInterval
-    var arrivalTime: TimeInterval
-    var flyDuration: String
-    var airlines: [String]
-    var price: Int
-    var deepLink: String
+    var flyTo: String?
+    var flyFrom: String?
+    var cityTo: String?
+    var cityFrom: String?
+    var departureTime: TimeInterval?
+    var arrivalTime: TimeInterval?
+    var flyDuration: String?
+    var airlines: [String]?
+    var price: Int?
+    var deepLink: String?
     
     // MARK: - Initializer
     
-    init(flyTo: String, flyFrom: String, cityTo: String, cityFrom: String, departureTime: TimeInterval, arrivalTime: TimeInterval, flyDuration: String, airlines: [String], price: Int, deepLink: String) {
+    init(flyTo: String?, flyFrom: String?, cityTo: String?, cityFrom: String?, departureTime: TimeInterval?, arrivalTime: TimeInterval?, flyDuration: String?, airlines: [String]?, price: Int?, deepLink: String?) {
         self.flyTo = flyTo
         self.flyFrom = flyFrom
         self.cityTo = cityTo
@@ -36,5 +37,24 @@ class Flight {
         self.airlines = airlines
         self.price = price
         self.deepLink = deepLink
+    }
+    
+    // MARK: - Object Mapping
+    
+    required init?(map: Map) {
+        mapping(map: map)
+    }
+    
+    func mapping(map: Map) {
+        flyTo <- map["flyTo"]
+        flyFrom <- map["flyTrom"]
+        cityTo <- map["cityTo"]
+        cityFrom <- map["cityTrom"]
+        departureTime <- map["dTime"]
+        arrivalTime <- map["aTime"]
+        flyDuration <- map["fly_duration"]
+        airlines <- map["airlines"]
+        price <- map["price"]
+        deepLink <- map["deep_link"]
     }
 }
