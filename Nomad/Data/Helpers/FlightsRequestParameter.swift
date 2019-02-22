cadcb325637755171d1113c754ee6d10ecff7a41
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-22 17:39:55 +0100

    Create a new enum type 'FlightsRequestParameter' with different cases for the request parameters.

---
+++ Nomad/Data/Helpers/FlightsRequestParameter.swift
@@ -0,0 +1,16 @@
+//
+//  FlightsRequestParameter.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 22.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+enum FlightsRequestParameter: String {
+    case from = "fly_from"
+    case to = "fly_to"
+    case dateFrom = "date_from"
+    case dateTo = "date_to"
+    case limit = "limit"
+    case partner = "partner"
+}
