53dafb558878bcaed7ba138c8bcbc18b62cb05e0
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-24 16:59:44 +0100

    refactor: Refactor 'parseFlight' method. refactor: Refactor the response of the network call the fetch the flights data.

--- Nomad/Data/Remote/FlightsRemoteDataSource.swift
+++ Nomad/Data/Remote/FlightsRemoteDataSource.swift
@@ -26,15 +26,21 @@ class FlightsRemoteDataSource: FlightsDataSource {
     func flights(flightsFilter: FlightsFilter) -> Observable<[Flight]> {
         let from = flightsFilter.from
         let dateFrom = flightsFilter.dateFrom
-        let dateTo = flightsFilter.dateTo
+        let returnFrom = flightsFilter.returnFrom
+        let price = flightsFilter.price
         let limit = flightsFilter.limit
         let partner = Constants.DefaultFilter.partner
+        let nightsInDestinationFrom = Constants.DefaultFilter.nightsInDestinationFrom
+        let nightsInDestinationTo = Constants.DefaultFilter.nightsInDestinationTo
 
         let parameters: [String: Any] = [FlightsRequestParameter.from.rawValue: from,
                                          FlightsRequestParameter.dateFrom.rawValue: dateFrom,
-                                         FlightsRequestParameter.dateTo.rawValue: dateTo,
+                                         FlightsRequestParameter.returnFrom.rawValue: returnFrom,
+                                         FlightsRequestParameter.price.rawValue: price,
                                          FlightsRequestParameter.limit.rawValue: limit,
-                                         FlightsRequestParameter.partner.rawValue: partner]
+                                         FlightsRequestParameter.partner.rawValue: partner,
+                                         FlightsRequestParameter.nightsInDestinationFrom.rawValue: nightsInDestinationFrom,
+                                         FlightsRequestParameter.nightsInDestinationTo.rawValue: nightsInDestinationTo]
 
         return restNetworkClient.performRequest(requestURLString: API.Flights.endpointUrl, type: .get, parameters: parameters).flatMap { result in
             try self.parseFlightsQueryResponse(result: result)
@@ -49,11 +55,12 @@ extension FlightsRemoteDataSource {
         switch result {
         case .success(let response):
             guard let responseData = response.1 as? [String: Any],
-                let flightsData = responseData[API.Flights.ResponseKeys.data] as? [[String: Any]] else {
+                let flightsData = responseData[API.Flights.ResponseKeys.data] as? [[String: Any]],
+                let currency = responseData[API.Flights.ResponseKeys.currency] as? String else {
                     throw DataErrorHelper.parseError
             }
             let flights = flightsData.compactMap { flightData in
-                return self.parseFlight(flightData: flightData)
+                return self.parseFlight(flightData: flightData, currency: currency)
             }.compactMap { $0 }
             return Observable<[Flight]>.of(flights)
         case .failure(let error):
@@ -61,7 +68,9 @@ extension FlightsRemoteDataSource {
         }
     }
 
-    private func parseFlight(flightData: [String: Any]) -> Flight? {
-        return Flight(JSON: flightData)
+    private func parseFlight(flightData: [String: Any], currency: String?) -> Flight? {
+        let flight = Flight(JSON: flightData)
+        flight?.currency = currency
+        return flight
     }
 }
