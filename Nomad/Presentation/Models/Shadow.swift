774ea68dc583008c92ac521ffec373172bb071c8
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-24 16:56:21 +0100

    Add a new struct for the shadow to hold the shadow properties.

---
+++ Nomad/Presentation/Models/Shadow.swift
@@ -0,0 +1,18 @@
+//
+//  Shadow.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 24.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+import UIKit
+
+struct Shadow {
+    let radius: CGFloat
+    let opacity: Float
+    let offset: CGSize
+    let masksToBounds: Bool
+    let shadowColor: UIColor
+}
+
