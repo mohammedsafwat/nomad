7f31a8024afb570716acfa796081d9e4e047a6cc
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-24 22:09:43 +0100

    Add a new enum type 'StoryboardSegue' to hold the values of the different segues.

---
+++ Nomad/Presentation/Helpers/StoryboardSegue.swift
@@ -0,0 +1,11 @@
+//
+//  StoryboardSegue.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 24.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+enum StoryboardSegue: String {
+    case flightsToFilters = "FlightsToFilterSegue"
+}
