836c206fc271428dfa0d9662698209e4aa35b8db
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-28 01:09:28 +0100

    Create a new 'LocationsViewController' class.

---
+++ Nomad/Presentation/Scenes/Locations/ViewControllers/LocationsViewController.swift
@@ -0,0 +1,63 @@
+//
+//  LocationsViewController.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 26.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class LocationsViewController: UIViewController {
+
+    // MARK: - Properties
+
+    @IBOutlet private weak var locationsSearchTextField: UITextField!
+    @IBOutlet private weak var locationsTableView: UITableView!
+
+    private let schedulersFacade = SchedulersFacade()
+    private let disposeBag = DisposeBag()
+    private(set) var selectedLocation = PublishSubject<Location>()
+
+    private lazy var viewModel: LocationsViewModel = {
+        let locationsDataSource = DataModule.shared.locationsRepository()
+        return LocationsViewModel(locationsDataSource: locationsDataSource, schedulersFacade: schedulersFacade)
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Setup UI
+        setupLocationsSearchTextField()
+        setupLocationsTableView()
+
+        // Data Binding
+        viewModel.locationsSearchInput = locationsSearchTextField.rx.text.map { $0 }
+        viewModel.locations
+            .asDriver(onErrorJustReturn: [])
+            .drive(locationsTableView.rx.items(cellIdentifier: Constants.ViewControllers.Locations.LocationsTableView.cellIdentifier, cellType: LocationsTableViewCell.self)) { _, location, cell in
+                cell.configure(location: location)
+        }.disposed(by: disposeBag)
+
+        locationsTableView.rx.modelSelected(Location.self)
+            .subscribe(onNext: { [weak self] location in
+                self?.selectedLocation.onNext(location)
+                self?.navigationController?.popViewController(animated: true)
+            }).disposed(by: disposeBag)
+    }
+}
+
+// MARK: - Private Methods
+
+extension LocationsViewController {
+    private func setupLocationsSearchTextField() {
+        locationsSearchTextField.placeholder = Constants.ViewControllers.Locations.locationsSearchTextFieldPlaceholder
+        locationsSearchTextField.font = UIFont(name: Constants.GeneralProperties.fontName, size: Constants.GeneralProperties.fontSize) ?? UIFont.systemFont(ofSize: Constants.GeneralProperties.fontSize)
+    }
+
+    private func setupLocationsTableView() {
+        locationsTableView.tableFooterView = UIView()
+        locationsTableView.register(UINib(nibName: Constants.ViewControllers.Locations.LocationsTableView.cellName, bundle: .main), forCellReuseIdentifier: Constants.ViewControllers.Locations.LocationsTableView.cellIdentifier)
+    }
+}
