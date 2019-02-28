3738dd0c795519a76d935fe5c72d5298afcfd5c1
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-28 08:13:27 +0100

    Create a new class method to return weekend dates as strings.

--- Nomad/Data/Utils/DateUtils/DateUtils.swift
+++ Nomad/Data/Utils/DateUtils/DateUtils.swift
@@ -12,6 +12,20 @@ class DateUtils {
     class func stringFromDate(date: Date, dateStyle: DateFormatter.Style = DateFormatter.Style.medium, timeStyle: DateFormatter.Style = DateFormatter.Style.none, dateFormat: String? = nil) -> String {
         return dateFormatter(dateStyle: dateStyle, timeStyle: timeStyle, dateFormat: dateFormat).string(from: date)
     }
+
+    class func weekendDateStrings(travelInterval: TravelInterval) -> (String, String) {
+        switch travelInterval {
+        case .thisWeekend:
+            return (Date().thisWeekendDates(format: Constants.GeneralProperties.dateFormat).0,
+                    Date().thisWeekendDates(format: Constants.GeneralProperties.dateFormat).1)
+        case .nextWeekend:
+            return (Date().nextWeekendDates(format: Constants.GeneralProperties.dateFormat).0,
+                    Date().nextWeekendDates(format: Constants.GeneralProperties.dateFormat).1)
+        case .theWeekendAfter:
+            return (Date().nextTwoWeekendsDates(format: Constants.GeneralProperties.dateFormat).0,
+                    Date().nextTwoWeekendsDates(format: Constants.GeneralProperties.dateFormat).1)
+        }
+    }
 }
 
 // MARK: - Private Methods
