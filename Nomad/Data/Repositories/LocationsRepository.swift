a285716a6c6ca3e0d03a49a664384aad761fc40f
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-28 01:16:53 +0100

    Create a new 'LocationsRepository' class with a 'remoteDataSource' property.

---
+++ Nomad/Data/Repositories/LocationsRepository.swift
@@ -0,0 +1,28 @@
+//
+//  LocationsRepository.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 27.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+import RxSwift
+
+class LocationsRepository: LocationsDataSource {
+
+    // MARK: - Properties
+
+    private var remoteDataSource: LocationsDataSource
+
+    // MARK: - Initializer
+
+    init(remoteDataSource: LocationsDataSource) {
+        self.remoteDataSource = remoteDataSource
+    }
+
+    // MARK: - LocationsDataSource Methods
+
+    func locations(name: String) -> Observable<[Location]> {
+        return remoteDataSource.locations(name: name)
+    }
+}
