ca48c071ce9c0dad23fdd3fe7d534b73a47bc40a
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-28 01:49:01 +0100

    Create a new enum type 'LocationsRequestParameter'.

---
+++ Nomad/Data/Helpers/LocationsRequestParameter.swift
@@ -0,0 +1,16 @@
+//
+//  LocationsRequestParameter.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 27.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+enum LocationsRequestParameter: String {
+    case term = "term"
+    case locationTypes = "location_types"
+    case activeOnly = "active_only"
+    case sort = "sort"
+}
+
+
