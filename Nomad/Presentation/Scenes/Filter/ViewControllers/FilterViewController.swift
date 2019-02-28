e308bbc2d70cd9b1fea5fca93a8b2cdc0bb8795e
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-28 01:27:37 +0100

    Implement all FilterViewController rows and update the flightsFilter value when one of the filter parameters change.

--- Nomad/Presentation/Scenes/Filter/ViewControllers/FilterViewController.swift
+++ Nomad/Presentation/Scenes/Filter/ViewControllers/FilterViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Eureka
+import RxSwift
+import RxCocoa
 
 class FilterViewController: FormViewController {
 
@@ -15,12 +17,42 @@ class FilterViewController: FormViewController {
 
     @IBOutlet private weak var doneBarButtonItem: UIBarButtonItem!
 
+    private let disposeBag = DisposeBag()
+    private(set) lazy var viewModel: FilterViewModel = {
+        return FilterViewModel()
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Setup UI
         setupDoneBarButtonItem()
         setupViewControllerTitle()
         setupViewControllerSections()
+
+        // Setup Data Binding
+        viewModel.flightsFilter.subscribe(onNext: { [weak self] flightsFilter in
+            self?.updateDepartureRowTitle(title: flightsFilter?.from.name)
+        }).disposed(by: disposeBag)
+    }
+}
+
+// MARK: - Segues
+
+extension FilterViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+
+        switch storyboardSegue(for: segue) {
+        case .filterToLocations:
+            let locationsViewController = segue.destination as? LocationsViewController
+            locationsViewController?.selectedLocation.subscribe(onNext: { [weak self] location in
+                self?.updateDepartureRowTitle(title: location.name)
+                self?.viewModel.flightsFilter.value?.from = location
+            }).disposed(by: disposeBag)
+        default:
+            break
+        }
     }
 }
 
@@ -32,23 +64,46 @@ extension FilterViewController {
     }
 
     private func setupViewControllerTitle() {
-        self.title = "Filter"
+        self.title = Constants.ViewControllers.Filter.viewControllerTitle
     }
 
     private func setupViewControllerSections() {
         form
 
         +++ Section(Constants.ViewControllers.Filter.Sections.DepartureSection.departureSectionTitle)
-        <<< TextRow() { row in
-            row.placeholder = Constants.ViewControllers.Filter.Sections.DepartureSection.placeholder
-        }
+        <<< LocationsPushRow<String>() {
+                $0.title = Constants.ViewControllers.Filter.Sections.DepartureSection.placeholder
+            }.cellSetup { cell, row in
+                cell.textLabel?.font = UIFont(name: Constants.GeneralProperties.fontName, size: Constants.GeneralProperties.fontSize) ?? UIFont.systemFont(ofSize: Constants.GeneralProperties.fontSize)
+            }
 
         +++ Section(Constants.ViewControllers.Filter.Sections.TravelIntervalSection.travelIntervalSectionTitle)
         <<< ActionSheetRow<String>() {
-            $0.title = Constants.ViewControllers.Filter.Sections.TravelIntervalSection.title
-            $0.selectorTitle = Constants.ViewControllers.Filter.Sections.TravelIntervalSection.selectorTitle
-            $0.options = Constants.ViewControllers.Filter.Sections.TravelIntervalSection.options
-            $0.value = Constants.ViewControllers.Filter.Sections.TravelIntervalSection.options[0]    // initially selected
+                $0.title = Constants.ViewControllers.Filter.Sections.TravelIntervalSection.title
+                $0.selectorTitle = Constants.ViewControllers.Filter.Sections.TravelIntervalSection.selectorTitle
+                $0.options = Constants.ViewControllers.Filter.Sections.TravelIntervalSection.options
+                $0.value = Constants.ViewControllers.Filter.Sections.TravelIntervalSection.options[0]
+            }.cellSetup { cell, row in
+                cell.textLabel?.font = UIFont(name: Constants.GeneralProperties.fontName, size: Constants.GeneralProperties.fontSize) ?? UIFont.systemFont(ofSize: Constants.GeneralProperties.fontSize)
+            }
+
+        +++ Section(Constants.ViewControllers.Filter.Sections.PriceSection.priceSectionTitle)
+        <<< SliderRow() {
+            $0.value = Float(Constants.DefaultFilter.price)
+            $0.shouldHideValue = false
+            $0.steps = Constants.ViewControllers.Filter.Sections.PriceSection.sliderSteps
+        }.cellSetup { cell, row in
+            cell.slider.minimumValue = Constants.ViewControllers.Filter.Sections.PriceSection.sliderMinimumValue
+            cell.slider.maximumValue = Constants.ViewControllers.Filter.Sections.PriceSection.sliderMaximumValue
+            cell.valueLabel.font = UIFont(name: Constants.GeneralProperties.fontName, size: Constants.GeneralProperties.fontSize) ?? UIFont.systemFont(ofSize: Constants.GeneralProperties.fontSize)
+        }.cellUpdate { [weak self] cell, row in
+            self?.viewModel.flightsFilter.value?.price = Int(row.value ?? Float(Constants.DefaultFilter.price))
+        }
+    }
+
+    private func updateDepartureRowTitle(title: String?) {
+        if let locationsPushRow = self.form.allRows.first as? LocationsPushRow<String> {
+            locationsPushRow.title = title
         }
     }
 }
@@ -57,6 +112,21 @@ extension FilterViewController {
 
 extension FilterViewController {
     @objc private func didTapDoneBarButtonItem() {
+        viewModel.flightsFilter.accept(viewModel.flightsFilter.value)
         dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - Injectable Protocol
+
+extension FilterViewController: Injectable {
+    typealias T = FlightsFilter
+
+    func inject(_ flightsFilter: T?) {
+        viewModel.flightsFilter.accept(flightsFilter)
+    }
+
+    func assertDependencies() {
+        assert(viewModel.flightsFilter.value != nil, "`inject` method on FilterViewController should be called")
     }
 }
