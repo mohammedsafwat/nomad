f834ecff7484c6a3d51c380e393fb2fc2b722469
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-02-28 01:45:33 +0100

    Create a new 'TravelInterval' enum with four different cases.

---
+++ Nomad/Data/Helpers/TravelInterval.swift
@@ -0,0 +1,14 @@
+//
+//  TravelInterval.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 28.02.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+enum TravelInterval: String {
+    case thisWeekend = "This Weekend"
+    case nextWeekend = "Next Weekend"
+    case theWeekendAfter = "The Weekend After"
+    case later = "Later"
+}
