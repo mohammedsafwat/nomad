93fd1a54ea5ae859886ef6ae589ad48f116f97c2
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-03-01 17:14:03 +0100

    Create a new struct for StorUtilsKeys.

---
+++ Nomad/Data/Utils/StoreUtils/StoreUtilsKeys.swift
@@ -0,0 +1,13 @@
+//
+//  StoreUtilsKeys.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 01.03.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+import Foundation
+
+struct StoreUtilsKeys {
+    static let KEY_CURRENCY = "currency"
+}
