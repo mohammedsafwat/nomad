d8025a91e3eb7714fd91882fa9c582d40be4c3e5
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-03-05 23:17:46 +0100

    Add some 'MARK' sections.

--- Nomad/Data/Modules/HttpClientModule.swift
+++ Nomad/Data/Modules/HttpClientModule.swift
@@ -9,8 +9,13 @@
 import Foundation
 
 class HttpClientModule {
+
+    // MARK: - Properties
+    
     static let shared = HttpClientModule()
     var httpClient: RestNetworkClientProtocol
+
+    // MARK: - Initializer
 
     private init() {
         httpClient = RestNetworkClient(headers: [:])
