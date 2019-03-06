3a26817dbccbc913b4dcdbe90a89cbd2b4e2425a
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-03-06 21:51:50 +0100

    Major refactoring inside 'FlightsLocalDataSource'.

--- Nomad/Data/DataSources/Local/FlightsLocalDataSource.swift
+++ Nomad/Data/DataSources/Local/FlightsLocalDataSource.swift
@@ -16,37 +16,80 @@ class FlightsLocalDataSource: FlightsDataSource {
     // MARK: - Properties
 
     private let managedObjectContext: NSManagedObjectContext
-    private let storeUtils: StoreUtils
+    private let storeUtils: StoreUtilsProtocol
 
     // MARK: - Initializer
 
-    init(managedObjectContext: NSManagedObjectContext, storeUtils: StoreUtils) {
+    init(managedObjectContext: NSManagedObjectContext, storeUtils: StoreUtilsProtocol) {
         self.managedObjectContext = managedObjectContext
         self.storeUtils = storeUtils
     }
 
     func flights(flightsFilter: FlightsFilter) -> Observable<FlightsResponse> {
-        let fetchRequest: NSFetchRequest<FlightEntity> = FlightEntity.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: Constants.DataSources.FlightsLocalDataSource.predicateFormatWithFilterId, flightsFilter.hashValue)
-        let priceSortDescriptor = NSSortDescriptor(key: Constants.DataSources.FlightsLocalDataSource.priceSortDescriptorKey, ascending: true)
-        fetchRequest.sortDescriptors = [priceSortDescriptor]
+        return Observable<FlightsResponse>.create { observer -> Disposable in
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: Constants.CoreData.FlightEntityName)
+            let filterIdPredicate = NSPredicate(format: Constants.DataSources.FlightsLocalDataSource.predicateFormatWithFilterId, String(format: "%d", flightsFilter.hashValue))
+            let priceSortDescriptor = NSSortDescriptor(key: Constants.DataSources.FlightsLocalDataSource.priceSortDescriptorKey, ascending: true)
+            fetchRequest.predicate = filterIdPredicate
+            fetchRequest.sortDescriptors = [priceSortDescriptor]
 
-        return managedObjectContext.rx.entities(fetchRequest: fetchRequest).asObservable().map { flightsManagedObjects in
-            flightsManagedObjects.compactMap { self.createFlight(from: $0) }
-        }.flatMap { flights -> Observable<FlightsResponse> in
-            let flightsResponse = FlightsResponse(flights: flights, currency: self.storeUtils.loadCurrency())
-            return Observable.of(flightsResponse)
+            do {
+                let flightsManagedObjects = try self.managedObjectContext.fetch(fetchRequest) as? [FlightEntity]
+                let flights = flightsManagedObjects?.compactMap { self.createFlight(from: $0) }
+                observer.onNext(FlightsResponse(flights: flights, currency: self.storeUtils.loadCurrency()))
+                observer.onCompleted()
+            } catch let error {
+                observer.onError(error)
+            }
+            return Disposables.create()
         }
     }
 
-    func storeFlights(flights: [Flight], withFilter flightsFilter: FlightsFilter) {
-
+    func storeFlights(flightsResponse: FlightsResponse, withFilter flightsFilter: FlightsFilter) -> Observable<FlightsResponse> {
+        return self.deleteAllFlights(withFlightsFilter: flightsFilter).flatMap { result -> Observable<FlightsResponse> in
+            return Observable<FlightsResponse>.create { observer -> Disposable in
+                switch result {
+                case .success:
+                    flightsResponse.flights?.forEach { self.saveFlight(flight: $0, flightsFilter: flightsFilter, completion: { dataError in
+                        if let dataError = dataError {
+                            observer.onError(dataError)
+                        }
+                    }) }
+                case .failure(let error):
+                    observer.onError(error)
+                }
+                
+                do {
+                    try self.managedObjectContext.save()
+                    observer.onNext(flightsResponse)
+                    observer.onCompleted()
+                } catch let error {
+                    let dataError = DataError(dataErrorType: .dbFailed, dataErrorMessage: error.localizedDescription)
+                    observer.onError(dataError)
+                }
+                return Disposables.create()
+            }
+        }
     }
 }
 
 // MARK: - Private Methods
 
 extension FlightsLocalDataSource {
+    private func saveFlight(flight: Flight, flightsFilter: FlightsFilter, completion: @escaping (DataError?) -> Void) {
+        managedObjectContext.performAndWait {
+            let flightManagedObject = FlightEntity(context: self.managedObjectContext)
+
+            flightManagedObject.price = Int64(flight.price ?? 0)
+            flightManagedObject.deepLink = flight.deepLink
+            flightManagedObject.filterId = String(format: "%d", flightsFilter.hashValue)
+            let routeItems = flight.route?.compactMap { self.createRouteItemManagedObject(from: $0) }
+            flightManagedObject.routeItems = NSSet(array: routeItems ?? [])
+
+            completion(nil)
+        }
+    }
+
     private func createFlight(from flightManagedObject: FlightEntity?) -> Flight {
         let routeItems = flightManagedObject?.routeItems?.allObjects as? [RouteItemEntity]
         let route = routeItems?.compactMap { self.createRouteItem(from: $0) }
@@ -69,45 +112,39 @@ extension FlightsLocalDataSource {
         return RouteItem(flyTo: flyTo, flyFrom: flyFrom, cityTo: cityTo, cityFrom: cityFrom, departureTime: departureTime, arrivalTime: arrivalTime, flightNumber: flightNumber, airline: airline)
     }
 
-    private func saveFlight(flight: Flight, withFilter flightsFilter: FlightsFilter, completion: @escaping (DataError?) -> Void) {
-        let predicate = NSPredicate(format: Constants.DataSources.FlightsLocalDataSource.predicateFormatWithFilterId, flightsFilter.hashValue)
-
-        managedObjectContext.perform {
-            self.getFlightManagedObject(withPredicate: predicate, completion: { result in
-                switch result {
-                case .success(let flightEntity):
-                    if let price = flight.price {
-                        flightEntity.price = Int64(price)
-                    }
-                    flightEntity.deepLink = flight.deepLink
-                    flightEntity.filterId = Int64(flightsFilter.hashValue)
-                    
-                    do {
-                        try self.managedObjectContext.save()
-                        completion(nil)
-                    } catch {
-                        completion(DataErrorHelper.dbFailedError)
-                    }
-                case .failure(let dataError):
-                    completion(dataError)
-                }
-            })
-        }
+    private func createRouteItemManagedObject(from routeItem: RouteItem) -> RouteItemEntity {
+        let routeItemManagedObject = RouteItemEntity(context: managedObjectContext)
+        routeItemManagedObject.flyTo = routeItem.flyTo
+        routeItemManagedObject.flyFrom = routeItem.flyFrom
+        routeItemManagedObject.cityTo = routeItem.cityTo
+        routeItemManagedObject.cityFrom = routeItem.cityFrom
+        routeItemManagedObject.departureTime = routeItem.departureTime ?? 0.0
+        routeItemManagedObject.arrivalTime = routeItem.arrivalTime ?? 0.0
+        routeItemManagedObject.flightNumber = Int64(routeItem.flightNumber ?? 0)
+        routeItemManagedObject.airline = routeItem.airline
+        return routeItemManagedObject
     }
 
-    private func getFlightManagedObject(withPredicate predicate: NSPredicate, completion: @escaping (Result<FlightEntity, DataError>) -> Void) {
-        let fetchRequest: NSFetchRequest<FlightEntity> = FlightEntity.fetchRequest()
-        fetchRequest.predicate = predicate
-        fetchRequest.sortDescriptors = []
+    private func deleteAllFlights(withFlightsFilter flightsFilter: FlightsFilter) -> Observable<Result<Bool, DataError>> {
+        return Observable<Result<Bool, DataError>>.create { observer -> Disposable in
+            let fetchRequest: NSFetchRequest<FlightEntity> = FlightEntity.fetchRequest()
+            let filterIdPredicate = NSPredicate(format: Constants.DataSources.FlightsLocalDataSource.predicateFormatWithFilterId, String(format: "%d", flightsFilter.hashValue))
+            fetchRequest.predicate = filterIdPredicate
 
-        do {
-            guard let flightManagedObject = try fetchRequest.execute().first else {
-                completion(.failure(DataErrorHelper.dbFailedError))
-                return
+            self.managedObjectContext.performAndWait {
+                do {
+                    let flightsManagedObjects = try fetchRequest.execute()
+                    flightsManagedObjects.forEach { self.managedObjectContext.delete($0) }
+                    try self.managedObjectContext.save()
+
+                    observer.onNext(.success(true))
+                    observer.onCompleted()
+                } catch let error {
+                    let dataError = DataError(dataErrorType: .dbFailed, dataErrorMessage: error.localizedDescription)
+                    observer.onError(dataError)
+                }
             }
-            completion(.success(flightManagedObject))
-        } catch {
-            completion(.failure(DataErrorHelper.dbFailedError))
+            return Disposables.create()
         }
     }
 }
