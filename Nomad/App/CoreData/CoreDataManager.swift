1f0b72cf8bfbde6a54c4b1b3789cab38cd543c95
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-03-06 21:56:58 +0100

    Conform to 'CoreDataManagerProtocol' inside 'CoreDataManager'.

--- Nomad/App/CoreData/CoreDataManager.swift
+++ Nomad/App/CoreData/CoreDataManager.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class CoreDataManager {
+class CoreDataManager: CoreDataManagerProtocol {
 
     // MARK: - Properties
 
@@ -23,7 +23,7 @@ class CoreDataManager {
         setupNotificationHandling()
     }
 
-    func managedObjectContext() -> NSManagedObjectContext {
+    var managedObjectContext: NSManagedObjectContext {
         // Initialize Managed Object Context
         let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
 
@@ -38,53 +38,53 @@ class CoreDataManager {
         guard let modelURL = Bundle.main.url(forResource: self.modelName, withExtension: "momd") else {
             fatalError("Unable to Find Data Model")
         }
-
+        
         // Initialize Managed Object Model
         guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
             fatalError("Unable to Load Data Model")
         }
-
+        
         return managedObjectModel
     }()
-
+    
     private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
         // Initialize Persistent Store Coordinator
         let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
         // Helpers
         let fileManager = FileManager.default
         let storeName = "\(self.modelName).sqlite"
-
+        
         // URL Documents Directory
         let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
-
+        
         // URL Persistent Store
         let persistentStoreURL = documentsDirectoryURL.appendingPathComponent(storeName)
-
+        
         do {
             // Add Persistent Store
             try persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: persistentStoreURL, options: nil)
         } catch {
             fatalError("Unable to Add Persistent Store")
         }
-
+        
         return persistentStoreCoordinator
     }()
-
+    
     private func setupNotificationHandling() {
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(saveChanges(_:)), name: UIApplication.willTerminateNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(saveChanges(_:)), name: UIApplication.didEnterBackgroundNotification, object: nil)
     }
-
+    
     @objc private func saveChanges(_ notification: Notification) {
         saveChanges()
     }
-
+    
     private func saveChanges() {
-        guard managedObjectContext().hasChanges else { return }
-
+        guard managedObjectContext.hasChanges else { return }
+        
         do {
-            try managedObjectContext().save()
+            try managedObjectContext.save()
         } catch {
             print("Unable to Save Managed Object Context")
             print("\(error), \(error.localizedDescription)")
