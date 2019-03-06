7fdbea1e395aa30933eac8753fb00077e72b9012
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-03-06 21:49:31 +0100

    Add the implementation of 'loadFlightsLastUpdateDate' and 'storeFlightsLastUpdateDate' inside StoreUtils.

--- Nomad/Data/Utils/StoreUtils/StoreUtils.swift
+++ Nomad/Data/Utils/StoreUtils/StoreUtils.swift
@@ -16,4 +16,12 @@ struct StoreUtils: StoreUtilsProtocol {
     func storeCurrency(currency: String) {
         UserDefaults.standard.set(currency, forKey: StoreUtilsKeys.KEY_CURRENCY)
     }
+
+    func loadFlightsLastUpdateDate() -> Date {
+        return UserDefaults.standard.value(forKey: StoreUtilsKeys.KEY_FLIGHTS_LAST_UPDATE_DATE) as? Date ?? Date()
+    }
+
+    func storeFlightsLastUpdateDate(lastUpdate: Date) {
+        UserDefaults.standard.set(lastUpdate, forKey: StoreUtilsKeys.KEY_FLIGHTS_LAST_UPDATE_DATE)
+    }
 }
