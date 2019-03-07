ecd858766070de8027b8c9068da1e24759a0ae92
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-03-07 14:48:22 +0100

    Implement two new methods 'flightsFilter' and 'storeFlightsFilter' inside 'FilterLocalDataSource'.

--- Nomad/Data/DataSources/Local/FilterLocalDataSource.swift
+++ Nomad/Data/DataSources/Local/FilterLocalDataSource.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import CoreData
+import RxSwift
 
 class FilterLocalDataSource: FilterDataSource {
 
@@ -21,8 +22,39 @@ class FilterLocalDataSource: FilterDataSource {
         self.managedObjectContext = managedObjectContext
     }
 
-    func storeFlightsFilter(flightsFilter: FlightsFilter) {
+    func flightsFilter() -> Observable<FlightsFilter> {
+        return Observable<FlightsFilter>.create { observer -> Disposable in
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: Constants.CoreData.FlightsFilterEntityName)
+            do {
+                let flightsFilterManagedObjects = try self.managedObjectContext.fetch(fetchRequest) as? [FlightsFilterEntity]
+                let flightsFilter = flightsFilterManagedObjects?.isEmpty ?? true ? DataModule.shared.defaultFlightsFilter : self.createFlightsFilter(from: flightsFilterManagedObjects?.first)
+                observer.onNext(flightsFilter)
+                observer.onCompleted()
+            } catch let error {
+                observer.onError(error)
+            }
+            return Disposables.create()
+        }
+    }
 
+    func storeFlightsFilter(flightsFilter: FlightsFilter) -> Observable<FlightsFilter> {
+        return self.deleteFlightsFilter().flatMap({ result -> Observable<FlightsFilter> in
+            return Observable<FlightsFilter>.create { observer -> Disposable in
+                switch result {
+                case .success:
+                    self.saveFlightsFilter(flightsFilter: flightsFilter) { dataError in
+                        if let dataError = dataError {
+                            observer.onError(dataError)
+                        }
+                        observer.onNext(flightsFilter)
+                        observer.onCompleted()
+                    }
+                case .failure(let error):
+                    observer.onError(error)
+                }
+                return Disposables.create()
+            }
+        })
     }
 }
 
@@ -49,11 +81,49 @@ extension FilterLocalDataSource {
         }
     }
 
+    private func deleteFlightsFilter() -> Observable<Result<Bool, DataError>> {
+        return Observable<Result<Bool, DataError>>.create { observer -> Disposable in
+            let fetchRequest: NSFetchRequest<FlightsFilterEntity> = FlightsFilterEntity.fetchRequest()
+
+            self.managedObjectContext.perform {
+                do {
+                    let flightsFilterManagedObjects = try fetchRequest.execute()
+                    flightsFilterManagedObjects.forEach { self.managedObjectContext.delete($0) }
+                    try self.managedObjectContext.save()
+                    observer.onNext(.success(true))
+                    observer.onCompleted()
+                } catch let error {
+                    let dataError = DataError(dataErrorType: .dbFailed, dataErrorMessage: error.localizedDescription)
+                    observer.onError(dataError)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
     private func createLocationManagedObject(from location: Location) -> LocationEntity {
         let locationManagedObject = LocationEntity(context: managedObjectContext)
         locationManagedObject.id = location.id
         locationManagedObject.code = location.code
         locationManagedObject.name = location.name
         return locationManagedObject
+    }
+
+    private func createFlightsFilter(from flightsFilterManagedObject: FlightsFilterEntity?) -> FlightsFilter {
+        let from = createLocation(from: flightsFilterManagedObject?.from)
+        let price = Int(flightsFilterManagedObject?.price ?? 0)
+        let limit = Int(flightsFilterManagedObject?.limit ?? 0)
+        let maxStopovers = Int(flightsFilterManagedObject?.maxStopovers ?? 0)
+        let travelIntervalString = flightsFilterManagedObject?.travelInterval ?? ""
+        let travelInterval = TravelInterval(rawValue: travelIntervalString) ?? Constants.DefaultFilter.travelInterval
+        return FlightsFilter(from: from, travelInterval: travelInterval == .undefined ? Constants.DefaultFilter.travelInterval : travelInterval, price: price, limit: limit, maxStopovers: maxStopovers)
+    }
+
+    private func createLocation(from locationManagedObject: LocationEntity?) -> Location {
+        let id = locationManagedObject?.id
+        let code = locationManagedObject?.code
+        let name = locationManagedObject?.name
+
+        return Location(id: id, code: code, name: name)
     }
 }
