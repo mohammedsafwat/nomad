2e164b560ba3430965f55d31a2bf4c222560acdb
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-03-07 22:37:22 +0100

    Create a new enum type for mock response.

---
+++ NomadTests/Data/Helpers/MockResponseType.swift
@@ -0,0 +1,12 @@
+//
+//  MockResponseType.swift
+//  NomadTests
+//
+//  Created by Mohammed Safwat on 07.03.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+enum MockResponseType {
+    case empty
+    case nonEmpty
+}
