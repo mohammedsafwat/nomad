3584493ba9c05a384ce079b62341a8db20edfd0a
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-03-07 05:32:42 +0100

    Create a new 'FilterDataSource' protocol.

---
+++ Nomad/Data/Protocols/FilterDataSource.swift
@@ -0,0 +1,11 @@
+//
+//  FilterDataSource.swift
+//  Nomad
+//
+//  Created by Mohammed Safwat on 07.03.19.
+//  Copyright © 2019 Mohammed Safwat. All rights reserved.
+//
+
+protocol FilterDataSource {
+    func storeFlightsFilter(flightsFilter: FlightsFilter)
+}
