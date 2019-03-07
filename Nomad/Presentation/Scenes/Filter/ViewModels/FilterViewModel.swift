2aeae11342f7f19f97876350b3d6bb2029823bae
Mohammed Safwat <m.safwat@holidaypirates.com>
2019-03-07 14:41:35 +0100

    Major refactoring inside 'FilterViewModel'.

--- Nomad/Presentation/Scenes/Filter/ViewModels/FilterViewModel.swift
+++ Nomad/Presentation/Scenes/Filter/ViewModels/FilterViewModel.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import RxCocoa
+import RxSwift
 
 struct FilterViewModel {
 
@@ -15,19 +16,36 @@ struct FilterViewModel {
 
     private let filterDataSource: FilterDataSource
     private let storeUtils: StoreUtilsProtocol
-
-    private(set) var flightsFilter = BehaviorRelay<FlightsFilter?>(value: nil)
-
-    // MARK: - Initializer
-
-    init(filterDataSource: FilterDataSource, storeUtils: StoreUtilsProtocol) {
-        self.filterDataSource = filterDataSource
-        self.storeUtils = storeUtils
-    }
+    private let schedulersFacade: SchedulersFacade
+    private(set) var flightsFilter: BehaviorRelay<FlightsFilter>
 
     var priceSectionTitle: String {
         return String(format: "%@ (%@)",
                       Constants.ViewControllers.Filter.Sections.PriceSection.priceSectionTitle,
                       storeUtils.loadCurrency())
+    }
+
+    var filter: Observable<FlightsFilter> {
+        return flightsFilter
+            .subscribeOn(schedulersFacade.backgroundScheduler())
+            .flatMap { filterValue -> Observable<FlightsFilter> in
+                return Observable.of(FlightsFilter(from: filterValue.from, travelInterval: filterValue.travelInterval, price: filterValue.price, limit: filterValue.limit, maxStopovers: filterValue.maxStopovers))
+            }
+    }
+
+    // MARK: - Initializer
+
+    init(filterDataSource: FilterDataSource, flightsFilter: BehaviorRelay<FlightsFilter>, storeUtils: StoreUtilsProtocol, schedulersFacade: SchedulersFacade) {
+        self.filterDataSource = filterDataSource
+        self.flightsFilter = flightsFilter
+        self.storeUtils = storeUtils
+        self.schedulersFacade = schedulersFacade
+    }
+
+    // MARK: - Store Filter
+
+    func storeFlightsFilter() -> Observable<FlightsFilter> {
+        return filterDataSource.storeFlightsFilter(flightsFilter: self.flightsFilter.value)
+            .subscribeOn(schedulersFacade.backgroundScheduler())
     }
 }
